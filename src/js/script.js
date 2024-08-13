887ce4e28abf939f3db2b31b4c10326107005d57
Artem <example@jmail.com>
2024-08-13 18:33:40 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// const answer = prompt('Сколько вам лет?');
+// console.log(answer);
+
+// if (answer < 18){
+//     alert('Уходите!')
+
+// } else {
+//     alert('Добро пожаловать!')
+// };
+
+const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+console.log(typeof(answers));
